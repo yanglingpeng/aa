211087dea30e790fab4484db5f2e237501bd9d72
haha <haha>
2016-05-23 17:30:40 +0800

    haha

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,12 @@
+class ArticlesController < ApplicationController
+	def  new
+	end
+end
+class ArticlesController < ApplicationController
+  def new
+  end
+ 
+  def create
+  	 render plain: params[:article].inspect
+  end
+end
